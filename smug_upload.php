11be035ab50bc1354553974bd0b9ba353bbe1791
oflannabhra <oflannabhra@gmail.com>
2014-07-27 23:42:06 -0400

    prevent dupes: TODO: check filename duplicatation bug

--- smug_upload.php
+++ smug_upload.php
@@ -1,33 +1,57 @@
 <?php
 
 require_once("phpSmug/phpSmug.php");
-const EMAIL = "ben.flannery@gmail.com";
-const PASS = "fl@n6o4o";
-const APIKEY = "hNOYUvAmnTaQrxkh27ePYEauYAu1Cwvs";
-const APPNAME = "uploadscript / 0.1 (http://www.oflannabhra.com";
-const UPLOAD_DIRECTORY = "/Users/meph/Pictures/Ben-Kate/";
-const UPLOAD_ALBUM_ID = "40941405";
+include("config.php");
 
-$sm = new phpSmug(array("APIKey" => APIKEY,
-   	        "AppName" => APPNAME,
-   	        "APIVer" => "1.2.2")
-   	       );
+
+
 
 try{
+	upload(setSession());
+}
+
+catch (Exception $e){
+	echo($e->message);
+	upload(setSession());
+}
+
+function setSession()
+{
+	$sm = new phpSmug(array("APIKey" => APIKEY,
+	"AppName" => APPNAME,
+	"APIVer" => "1.2.2")
+	);
 	$sm->login("EmailAddress=" . EMAIL, "Password=" . PASS);
 	$sm->setAdapter("socket");
+
+	return $sm;
+}
+function upload(phpSmug $sm)
+{
+
 	$files = getFiles(UPLOAD_DIRECTORY);
+	$album = $sm->images_get("AlbumID=".UPLOAD_ALBUM_ID, "AlbumKey=".UPLOAD_ALBUM_KEY);
+	
+	// Get a List of images from the album to check for dupes
+	$images = $album["Images"];
+	$names = array();
+
+	foreach ($images as $image) {
+		array_push($names, $sm->images_getInfo("ImageID=".$image['id'], "ImageKey=".$image['Key'])['FileName']);
+	}
+
 
 	foreach ($files as $file)
 	{
-		$sm->images_upload("AlbumID=".UPLOAD_ALBUM_ID, "File=".UPLOAD_DIRECTORY . $file);
-		echo ("Uploaded: " . $file . "\n");
+		if (array_search($file.', '.$file, $names)===FALSE){
+			$response =  $sm->images_upload("AlbumID=".UPLOAD_ALBUM_ID, "File=".UPLOAD_DIRECTORY . $file, "FileName=".$file);
+			var_dump($response);
+			echo ("\nUploaded: " . $file . "\n");
+		}
+		else {
+			echo ("\n{$file} already exists in the album");
+		}
 	}
-
-}
-
-catch (PhpSmugException $e){
-	exit;
 }
 
 function getFiles($directory){
